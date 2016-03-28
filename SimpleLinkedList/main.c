2cef7bae6011b0be7887b9db252cfad56db9d7c8
Eric Liu <eric.liu.developer@gmail.com>
2016-03-28 15:38:47 +1100

    reverse linked list

--- SimpleLinkedList/main.c
+++ SimpleLinkedList/main.c
@@ -22,6 +22,8 @@ void insertAtHead( struct Node** pointer2Head, int data);
 void insertAtIndex(struct Node** pointer2Head, int index, int data);
 void deleteAtIndex(struct Node** pointer2Head, int index);
 
+void reverseList(struct Node** pointer2Head);
+
 int main(int argc, const char * argv[]) {
     
     
@@ -43,6 +45,11 @@ int main(int argc, const char * argv[]) {
     
     printList(head);
     
+    printf("start reversing... \n");
+    
+    reverseList(&head);
+    
+    printList(head);
     
     return 0;
 }
@@ -113,7 +120,31 @@ void deleteAtIndex(struct Node** pointer2Head, int index){
     }else{
         printf(" \n =========== \n index out of bound!  \n =========== \n");
     }
+    
+}
 
+void reverseList(struct Node** pointer2Head){
+    struct Node* prev = NULL;
+    struct Node* current;
+    struct Node* next;
+    struct Node* head = *pointer2Head;
+    
+    current = head;
+    next = head;
+    
+    while ( next != NULL) {
+        current = next;
+        next = next -> next;
+        
+        current -> next = prev;
+        prev = current;
+        
+        
+        
+        // 1. start moving the index
+    }
+    *pointer2Head = current;
+    
 }
 
 
